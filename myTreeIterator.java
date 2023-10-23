466a0f03d43d36586623e224b397d539eb7edb46
Cleeps <89921789+Cleeps@users.noreply.github.com>
2023-10-23 14:39:28 -0400

    Update myTreeIterator.java
    Formatting shift

--- myTreeIterator.java
+++ myTreeIterator.java
@@ -5,14 +5,14 @@ import Lab5.BinarySearchTree.Node;
 public class myTreeIterator implements ITreeIterator 
 {
 
-  // Publicly accessible SequenceArray method
+  	// Publicly accessible SequenceArray method
 	public int[] SequenceArray(BinarySearchTree bst)
 	{
 		int[] sequenceArray = new int[bst.size];
 		return SequenceArray(bst.root, sequenceArray);
 	}
 
-  // Privately accessible SequenceArray method
+  	// Privately accessible SequenceArray method
 	private int[] SequenceArray(Node root, int[] list)
 	{
 		if (root == null)
@@ -20,20 +20,20 @@ public class myTreeIterator implements ITreeIterator
 			return null;
 		}
 
-    // Check the left subtree
+    		// Check the left subtree
 		SequenceArray(root.left, list);
 
-    // Required check to ensure that the first element of the array does not return -1
-    if(list.length > 0)
-    {
-      		list[list.length - 1] = root.data;
-    }
-    else
-    {
-      		list[0] = root.data;
-    }
+		    // Required check to ensure that the first element of the array does not return -1
+		    if(list.length > 0)
+		    {
+	      		list[list.length - 1] = root.data;
+		    }
+		    else
+		    {
+	      		list[0] = root.data;
+	    	    }
 
-    // Check the right subtree
+   	 	// Check the right subtree
 		SequenceArray(root.right, list);
 		
 		return list;
